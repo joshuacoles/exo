c6f2c6765b995285126a45d936742445fe763290
Joshua Coles <josh@coles.to>
2025-02-16 10:45:16 +0000

    Add `stop` to the OpenAI API and python side GenerationOptions and add custom `include_stop` option to `GenerationOptions`.

--- exo/inference/generation_options.py
+++ exo/inference/generation_options.py
@@ -3,6 +3,11 @@ from typing import Optional, List
 
 class GenerationOptions:
   max_completion_tokens: Optional[int] = None
+  stop: Optional[List[str]] = None
 
-  def __init__(self, max_completion_tokens: Optional[int] = None):
+  # Whether to include the stop sequence in the output, by default we do not include it. This does not apply to the stop token.
+  include_stop: bool = False
+
+  def __init__(self, max_completion_tokens: Optional[int] = None, stop: Optional[List[str]] = None):
     self.max_completion_tokens = max_completion_tokens
+    self.stop = stop
