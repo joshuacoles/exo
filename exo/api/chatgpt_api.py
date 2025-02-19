2d14b93956753ddb11b2f5da5acaa6a31b3d6784
Joshua Coles <josh@coles.to>
2025-02-19 13:23:25 +0000

    Fix issues when the tokens list is empty

--- exo/api/chatgpt_api.py
+++ exo/api/chatgpt_api.py
@@ -405,7 +405,7 @@ class ChatGPTAPI:
             if DEBUG >= 2: print(f"{eos_token_id=} {tokens[-1]=} {finish_reason=}")
 
             # We do not return the EOS token in the response
-            if tokens[-1] == eos_token_id:
+            if len(tokens) > 0 and tokens[-1] == eos_token_id:
               tokens.pop(-1)
 
             completion = generate_completion(
@@ -455,8 +455,7 @@ class ChatGPTAPI:
         eos_token_id = None
         if not eos_token_id and hasattr(tokenizer, "eos_token_id"): eos_token_id = tokenizer.eos_token_id
         if not eos_token_id and hasattr(tokenizer, "_tokenizer"): eos_token_id = tokenizer.special_tokens_map.get("eos_token_id")
-        if DEBUG >= 2: print(f"Checking if end of tokens result {tokens[-1]=} is {eos_token_id=}")
-        if tokens[-1] == eos_token_id:
+        if len(tokens) > 0 and tokens[-1] == eos_token_id:
           tokens.pop(-1)
 
         return web.json_response(generate_completion(chat_request, tokenizer, prompt, request_id, tokens, stream, finish_reason, "chat.completion"))
